e83618b180aeacb01dc874973cfe18ba715fb0ea
SriHarshaSwaraj <51262650+o0badboy0o@users.noreply.github.com>
2020-01-01 00:26:42 +0530

    Add files via upload

---
+++ ;.cpp
@@ -0,0 +1,15 @@
+#incldue<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_Stdio(false),cin.tie(NULL),cout.tie(NULL);
+    cout<<"Hello world.I want my Harsha back.Be it cg....habits...everything..i bloody want harsha back...where is that vaakchaturyam"\n;
+    cout<<"aa brain ekkada....brain asalu pani cheyyalta....its so numb...parledu..em parla....relax...slowly meditate..read..get back\n";
+    cout<<"aa confidence...aa focus...aa humour.....aa aura....no more time.....get back...this is my new year resolution\n";
+    cout<<"flush all shit thoughts....(get better)be the best at everything......ade harsha ante\n";
+    cout<<"go get paypal.....and then amazon....crack gsoc....remain busy...invest....invest...read red code code...succeed\n";
+    cout<<"neglected manushulu:- nani mamayya....pinnulu....nannan peddamma...anthe...get back frnds\n";
+    cout<<"konchem batuku......vedhava batuku batikindi chalu....ammananna ki konchem prema choopi....vedhava philosophies chalu\n";
+    return 0;
+}
